6789c3c75880431f8010a77d823c5e166b5c3edf
Finn Pauls <mbp@pauls.dev>
2019-04-27 14:26:43 +0200

    Updates

---
+++ precache-manifest.8a2d41e30de0e0fdfbd564d0c87f546d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131fa544f89ecc1a9925f87742755fe0",
+    "url": "/ld44/index.html"
+  },
+  {
+    "revision": "ba417a5bebc0403c2012",
+    "url": "/ld44/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ld44/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ba417a5bebc0403c2012",
+    "url": "/ld44/static/js/main.4aab6506.chunk.js"
+  },
+  {
+    "revision": "ada751b6aea07ab4abae",
+    "url": "/ld44/static/js/runtime~main.2aebaf0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ld44/static/media/logo.5d5d9eef.svg"
+  }
+]);
